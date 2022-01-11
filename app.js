b7fbdacb93f961a08629e01891f321ffcfe94ea9
Abhishek Singh <abhiwebdev2718@gmail.com>
2022-01-11 20:36:37 +0530

    job application working

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 require("dotenv").config();
 const express = require("express");
-const multer = require("multer");
+const multer = require("multer"),
+fileupload=require('express-fileupload');
 const cors = require("cors");
 const path = require("path");
 const app = express();
 app.use(cors({ origin: true, credentials: true }));
 app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ extended: true, limit: "100mb" }));
+ // set static folder
+ app.use(express.static('public'))
+ // file uploading
+ app.use(fileupload());
 require("./db");
 // // const { initializeApp } = require("firebase-admin/app");
 // const firebaseConfig = require("./config.js");
@@ -114,9 +119,52 @@ const payment = async (amount, req, res) => {
     console.log(error);
   }
 };
-
+app.post('/applyjob',async (req, res)=>{
+    try{
+        const application= await JobApply.create(req.body);
+        res.status(201).json({success:true,application:application})
+    } catch (error) {
+        res.status(408).json({success:true,message:"You already had applied for this job !"})
+    }
+})
+//================================================================================================================================
+app.post('/uploads',(req,res)=>{
+    function makeid(length) {
+        var result           = '';
+        var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        var charactersLength = characters.length;
+        for ( var i = 0; i < length; i++ ) {
+          result += characters.charAt(Math.floor(Math.random() * 
+     charactersLength));
+       }
+       return result;
+    }
+      if(!req.files){
+        return  res.status(400).json({status:false,msg:"Please upload an file"});    
+    }
+    const file=req.files.file;
+    // Checking file size unit is bytes,MAX_FILE_UPLOAD = 1 Megabyte which is equal to 10,00,000 bytes
+    if(file.size>process.env.MAX_FILE_UPLOAD_SIZE){
+        return res.status(400).json({status:false,msg:"Please upload an image less than 1 Megabyte"})
+    }
+    // Creating custom file name
+    file.name=`photo_${makeid(5)}${path.parse(file.name).ext}`;
+    file.mv(`./public/uploads/${file.name}`,async err=>{
+            if(err){
+                console.error(err);
+                return res.status(500).json({status:false,msg:"Problem with file upload"})
+            }
+            res.status(200).json({success:true,data:{
+                name:file.name,
+                url: `${req.protocol}://${req.get('host')}/uploads/${file.name}`
+            }});
+    })
+  })
+  app.get('/applied',async function(req,res){
+      const applications= await JobApply.find();
+      res.json({applications});
+  })
 app.use(express.static(path.join(__dirname, "/client/build")));
-
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "/client/build", "index.html"));
 });
