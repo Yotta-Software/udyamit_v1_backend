719ba7a459a97a77a3c1e19693253c962bb312f8
Abhishek Singh <abhiwebdev2718@gmail.com>
2022-01-13 01:42:07 +0530

    change in port

--- app.js
+++ app.js
@@ -2,19 +2,10 @@ require("dotenv").config();
 const express = require("express");
 const https = require("https");
 const fs = require("fs");
-const multer = require("multer"),
 fileupload=require('express-fileupload');
 const cors = require("cors");
 const path = require("path");
 const app = express();
-
-const options = {
-  key : fs.readFileSync('./greenlock.d/live/udyamit.in/privkey.pem'),
-  cert : fs.readFileSync('./greenlock.d/live/udyamit.in/cert.pem')
-}
-
-https.createServer(options, app).listen(8080, () => console.log('listening on port 8080'));
-
 app.use(cors({ origin: true, credentials: true }));
 app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ extended: true, limit: "100mb" }));
@@ -31,90 +22,13 @@ app.get('/',(req, res) => {
 const auth = require('./routes/auth');
 app.use('/api/v1/auth',auth);
 app.use(errorHandler);
-
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, __dirname + "/uploads");
-  },
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    cb(null, uniqueSuffix + "-" + file.originalname);
-  },
-});
-
-const upload = multer({ storage: storage });
-app.post("/upload", upload.single("file"), (req, res) => {
-  console.log(req.body, "file", req.file);
-  res.send({ ...req.file, ...req.body }).end();
-});
-
 app.use("/api", require("./routes/dataRoutes"));
-
-const Razorpay = require("razorpay");
-
-const razorpay = new Razorpay({
-  key_id: process.env.R_KEY,
-  key_secret: process.env.R_SECRATE,
-});
 
 const JOB = require("./models/Job");
 const DigitalLearning = require("./models/DigitalLearning.js");
 const JobApply = require("./models/JobApply.js");
 const Contactus = require("./models/Contactus.js");
-app.post("/pay", async (req, res) => {
-  try {
-    // const application = null;
-    if (req.body.type == "job") {
-      const application = await JobApply.findOne({
-        jobApplyId: req.body.id + req.body.email,
-      });
-      if (!application) {
-        return res.status(400).json({ msg: "apply first" });
-      }
 
-      if (application.category === "SC" || application.category === "PC") {
-        let job = await JOB.findOne({ _id: req.body.id });
-
-        if (!job) return res.status(400).json({ msg: "bad request" });
-
-        payment(job.application_fee_dis, req, res);
-      } else {
-        let job = await JOB.findOne({ _id: req.body.id });
-        console.log("jobid", job, req.body.id);
-        if (!job) return res.status(400).json({ msg: "bad request" });
-        payment(job.application_fee, req, res);
-      }
-    } else {
-      let job = await DigitalLearning.findOne({ _id: req.body.id });
-      console.log("jobid", job, req.body.id);
-      if (!job) return res.status(400).json({ msg: "bad request" });
-      payment(job.price, req, res);
-    }
-  } catch (er) {
-    console.log(er);
-  }
-});
-
-const payment = async (amount, req, res) => {
-  const options = {
-    amount: amount * 100,
-    currency: "INR",
-    receipt: "hftryukijhgutyujh78iou98766543567",
-    payment_capture: 1,
-  };
-
-  try {
-    const response = await razorpay.orders.create(options);
-    console.log(response);
-    res.json({
-      id: response.id,
-      currency: response.currency,
-      amount: response.amount,
-    });
-  } catch (error) {
-    console.log(error);
-  }
-};
 app.post('/applyjob',async (req, res)=>{
     try{
         const application= await JobApply.create(req.body);
@@ -208,5 +122,5 @@ app.use(express.static(path.join(__dirname, "/client/build")));
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "/client/build", "index.html"));
 });
-
+app.listen(8080,()=>console.log("listening on port 8080"));
 module.exports = app;
